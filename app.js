0dd2a98a6efa962f708a48d14758237a54514a09
dvdgrth <david.guerth@gmail.com>
2020-11-13 21:58:31 +0100

    Renamed entry file. Created routes folder.

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const DB = require("./databaseInterface");
+const indexRouter = require("./routes/index");
 const PORT = 3000;
 
 const app = express();
 
-(async function () {
-  await DB.initConnection();
-  console.log("after init");
-})();
+DB.initConnection();
 
-console.log("after init 2");
+app.use("/", indexRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(PORT, () =>
   console.log(`Server listening at http://localhost:${PORT}`)
